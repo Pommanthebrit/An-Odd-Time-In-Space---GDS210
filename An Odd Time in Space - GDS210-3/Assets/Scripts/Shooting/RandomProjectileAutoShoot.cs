77ce664f4159ad3000882414d7d7726c0fee872d
Geurtsy <36181392+Geurtsy@user.noreply.github.com>
2018-04-15 14:17:16 +0800

    Finished "Household Drone" base. Reorganised soup assets.

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Shooting/RandomProjectileAutoShoot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DATA_ShootingMechanic", menuName = "Shooting Mechanisms/Auto Random Projectile")]
+public class RandomProjectileAutoShoot : SimpleAutoShoot 
+{
+	[SerializeField] private GameObject[] _projectileList;
+
+	public override void Shoot()
+	{
+		base.Shoot();
+
+		_projectilePrefab = _projectileList[Random.Range(0, _projectileList.Length)];
+	}
+}
