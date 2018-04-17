def67eb2119690274e121b7ec809d2fecc955ad9
Geurtsy <geurtsy.gaming@gmail.coim>
2018-04-17 13:04:55 +0800

    BakerWindow Attempt. JediMode organise

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Editor/BakerWindow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class BakerWindow :  EditorWindow 
+{
+	[MenuItem("Window/Baker")]
+
+	private static void ShowWindow()
+	{
+		GetWindow<BakerWindow>("Baker");
+	}
+
+	private void OnGUI()
+	{
+		if(GUILayout.Button("Get lighting baked ;)"))
+		{
+			Lightmapping.Bake();
+		}
+	}
+}
