065e85dd445691e2e41dbad35279599a35badca5
Chris-Stoj <1010691@student.sae.edu.au>
2018-04-24 01:59:59 +0800

    Object picking up polish, soil pouring!

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Lobby/PourOut.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourOut : MonoBehaviour {
+
+	public GameObject myFilling;
+	public Transform[] pourSpawn;
+
+	void Update () {
+		if (Vector3.Dot(transform.up, Vector3.down) > 0f) {
+			Instantiate (myFilling, pourSpawn[Random.Range(0, pourSpawn.Length)].position, Quaternion.identity);
+		}
+	}
+}
