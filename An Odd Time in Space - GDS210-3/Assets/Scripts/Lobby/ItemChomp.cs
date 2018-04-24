f45fbddebfdc4b30fa34b8114b4078fc0e380943
Chris-Stoj <1010691@student.sae.edu.au>
2018-04-24 16:06:59 +0800

    GarbageBot Anims, item destroy and respawns

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Lobby/ItemChomp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemChomp : MonoBehaviour {
+
+	Animator chompAnim;
+	ParticleSystem chompPT;
+
+	void Start () {
+		chompAnim = GetComponent<Animator> ();
+		chompPT = GetComponent<ParticleSystem> ();
+	}
+	
+	void OnCollisionEnter (Collision collider) {
+		//Particles for eating items
+		if (collider.gameObject.tag == "Item") {
+			Invoke ("ChompItem", 0.01f);
+		}
+	}
+
+	void ChompItem () {
+		chompAnim.SetTrigger ("Chomp");
+		chompPT.Emit (40);
+	}
+}
---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Lobby/ItemRespawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRespawn : MonoBehaviour {
+
+	Transform initialPos;
+	Rigidbody _Rb;
+
+	void Start () {
+		initialPos = new GameObject ("InitialPosition").transform;
+		initialPos.position = transform.position;
+
+		_Rb = GetComponent<Rigidbody> ();
+	}
+		
+	void OnCollisionEnter (Collision collider) {
+		//Will only die if eaten by GarbageBot
+		if (collider.gameObject.tag == "Garbage") {
+			Invoke ("Respawn", 0.1f);
+		}
+	}
+
+	void Respawn () {
+		//Kills all momentum and respawns in original position
+		_Rb.velocity = Vector3.zero;
+		_Rb.angularVelocity = Vector3.zero;
+		transform.rotation = Quaternion.identity;
+		transform.position = initialPos.position;
+	}
+}
