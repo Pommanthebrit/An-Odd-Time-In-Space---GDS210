c4828287797df43b8e8702c7c7c4fb2ab4d8793d
CrimsonFury82 <1007626@student.sae.edu.au>
2018-04-24 13:25:15 +0800

    Aston's Update
    Added Robot model, Robot Script, Sound Effects

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Controllers/HubRobotController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HubRobotController : MonoBehaviour {
+
+	public Transform player;
+	Animator myRobotAnim;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		myRobotAnim = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Walk ();
+	}
+
+	public void Walk() {
+		myRobotAnim.SetBool ("IsWalking", true);
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.speed = speed;
+		agent.destination = player.position;
+	}
+
+	public void Idle() {
+		myRobotAnim.SetBool ("IsWalking", false);
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.speed = 0;
+	}
+
+	void OnCollisionEnter (Collision col){
+		if (col.gameObject.name == "Player") {
+			Idle ();
+		}
+	}
+
+}
