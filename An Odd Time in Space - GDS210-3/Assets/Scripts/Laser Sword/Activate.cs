45bb0e10e75d4c42c50acfea74fcaac6d8b341b1
kcrash201 <10089172@student.sae.edu.au>
2018-04-24 13:42:03 +0800

    Laser Sword Activate Script
    Needs to be triggered by grabbing function instead of instant

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Laser Sword/Activate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activate : MonoBehaviour {
+
+	public Material Purple;
+
+	// Use this for initialization
+	void Start () {
+		Material[] previousMaterials = GetComponent<MeshRenderer> ().materials;
+		previousMaterials [3] = Purple;
+		GetComponent<MeshRenderer> ().materials = previousMaterials;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
