cbd3c00ed717e5910ee60130f94658fd6993905e
Geurtsy <36181392+Geurtsy@users.noreply.github.com>
2018-04-25 23:07:10 +0800

    Meta files

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/BallHit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHit : MonoBehaviour
+{
+    [SerializeField] private float _hitForce;
+    [SerializeField] private float _pushRadius;
+    private Rigidbody _rb;
+    private Collider _clubCol;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            other.attachedRigidbody.AddExplosionForce(_hitForce, transform.position, _pushRadius);
+            Debug.Log(other);
+        }
+    }
+}
