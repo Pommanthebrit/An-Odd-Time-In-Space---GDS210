4730655e44610697bd5db3ab3a1928e3cb5c8023
Chris-Stoj <1010691@student.sae.edu.au>
2018-04-26 01:41:10 +0800

    Range scene work

--- An Odd Time in Space - GDS210-3/Assets/Scripts/BallHit.cs
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/BallHit.cs
@@ -5,20 +5,15 @@ using UnityEngine;
 public class BallHit : MonoBehaviour
 {
     [SerializeField] private float _hitForce;
+	[SerializeField] private float _torqueForce;
     [SerializeField] private float _pushRadius;
-    private Rigidbody _rb;
-    private Collider _clubCol;
-
-    private void Start()
-    {
-        _rb = GetComponent<Rigidbody>();
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Ball")
         {
             other.attachedRigidbody.AddExplosionForce(_hitForce, transform.position, _pushRadius);
+			other.attachedRigidbody.AddTorque (Vector3.forward, ForceMode.Impulse);
             Debug.Log(other);
         }
     }
