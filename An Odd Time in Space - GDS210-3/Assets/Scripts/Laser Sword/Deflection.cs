98c8923812eeca9a19d8d6a29622c3cb44a18608
kcrash201 <1008917@student.sae.edu.au>
2018-04-25 19:24:10 +0800

    Deflection implimentation
    Errors atm

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Laser Sword/Deflection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deflection : MonoBehaviour {
+
+	public bool collision;
+	public float numBounces;
+
+	void OnCollisionEnter(collision : Collision)
+	{
+		var contact : ContactPoint;
+		if(numBounces > 0  (collision.gameObject.layer != 8))
+		{
+			contact = collision.contacts[0];
+			var dot : float = Vector3.Dot(contact.normal, (-transform.forward));
+			dot *= 2;
+			reflection = contact.normal * dot;
+			reflection = reflection + transform.forward;
+			rigidbody.velocity = rigidbody.TransformDirection(reflection.normalized * 15.0);
+			numBounces -= 1;
+		}
+	}
+}
