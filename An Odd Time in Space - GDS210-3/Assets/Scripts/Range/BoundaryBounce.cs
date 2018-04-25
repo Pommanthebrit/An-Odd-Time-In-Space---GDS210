d769ce749beffa704f42a6665d08189d1d4f1226
Chris-Stoj <1010691@student.sae.edu.au>
2018-04-25 19:19:07 +0800

    Range boundary tweaks and effects

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Range/BoundaryBounce.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryBounce : MonoBehaviour {
+
+	public GameObject bounceParticle;
+
+	void OnCollisionEnter (Collision collider) {
+		Instantiate (bounceParticle, collider.transform.position, Quaternion.identity);
+	}
+}
