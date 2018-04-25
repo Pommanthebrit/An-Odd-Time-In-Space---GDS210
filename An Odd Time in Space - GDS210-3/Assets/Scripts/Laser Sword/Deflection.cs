79d41d552a9507bd349f30a645b6ace4e1e23fc1
Geurtsy <36181392+Geurtsy@users.noreply.github.com>
2018-04-25 23:01:40 +0800

    Deflection Translation

--- An Odd Time in Space - GDS210-3/Assets/Scripts/Laser Sword/Deflection.cs
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Laser Sword/Deflection.cs
@@ -2,24 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Deflection : MonoBehaviour {
+public class Deflection : MonoBehaviour
+{
+    public bool _collision;
+    public float _bounceNum;
+    private Rigidbody _rb;
 
-	public bool collision;
-	public float numBounces;
-	/*
-	void OnCollisionEnter(collision : Collision)
-	{
-		var contact : ContactPoint;
-		if(numBounces > 0  (collision.gameObject.layer != 8))
-		{
-			contact = collision.contacts[0];
-			var dot : float = Vector3.Dot(contact.normal, (-transform.forward));
-			dot *= 2;
-			reflection = contact.normal * dot;
-			reflection = reflection + transform.forward;
-			rigidbody.velocity = rigidbody.TransformDirection(reflection.normalized * 15.0);
-			numBounces -= 1;
-		}
-	}
-	*/
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact;
+        if(_bounceNum > 0 && collision.gameObject.layer != 8)
+        {
+            contact = collision.contacts[0];
+            float dot = Vector3.Dot(contact.normal, -transform.forward);
+            dot *= 2;
+
+            Vector3 reflection = contact.normal * dot;
+            reflection = reflection + transform.forward;
+
+            _rb.velocity = _rb.transform.TransformDirection(reflection.normalized * 15.0f);
+            _bounceNum -= 1;
+        }
+    }
 }
