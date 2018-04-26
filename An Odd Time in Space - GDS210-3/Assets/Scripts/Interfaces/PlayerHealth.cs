fb10d488bd9f1f2f91bd4769a19f42282aa2e0de
Chris-Stoj <1010691@student.sae.edu.au>
2018-04-27 05:40:40 +0800

    Jedi player health implemented

--- An Odd Time in Space - GDS210-3/Assets/Scripts/Interfaces/PlayerHealth.cs
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Interfaces/PlayerHealth.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour {
 
+	PlayerController _pC;
+
 	public int myPlayerHealth;
 	SpriteRenderer _spriteRenderer;
 	public Sprite heartFull, heartEmpty;
 
 
 	void Start () {
+		_pC = FindObjectOfType<PlayerController> ();
 		_spriteRenderer = GetComponent<SpriteRenderer> ();
 	}
 	
 	void Update () {
-		if (myPlayerHealth == 0) {
+		if (myPlayerHealth == _pC._health) {
 			_spriteRenderer.sprite = heartFull;
 		} else
 			_spriteRenderer.sprite = heartEmpty;
