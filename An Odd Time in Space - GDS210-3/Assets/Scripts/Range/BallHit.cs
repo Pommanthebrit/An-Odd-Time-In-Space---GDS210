dd9df89651c559be064971546064aaac81eeeaa2
Chris-Stoj <1010691@student.sae.edu.au>
2018-04-27 00:10:30 +0800

    Jedi orb tweak and range tweak

--- An Odd Time in Space - GDS210-3/Assets/Scripts/Range/BallHit.cs
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Range/BallHit.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class BallHit : MonoBehaviour
 {
     [SerializeField] private float _hitForce;
-	[SerializeField] private float _torqueForce;
     [SerializeField] private float _pushRadius;
 
     private void OnTriggerEnter(Collider other)
