4d53538e6b8c35c6652f611f5cce06410ca95453
Geurtsy <36181392+Geurtsy@users.noreply.github.com>
2018-04-26 22:32:35 +0800

    Pushing WazerStartup light fade

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Controllers/WazerStartup.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class WazerStartup : MonoBehaviour
+{
+    [SerializeField] private Light[] _lightsToFadeIn;
+    [SerializeField] private float _fadeTime;
+    [SerializeField] private float _maxLightRange;
+    [SerializeField] private GameGod[] _objectsToInsantiate;
+
+    public void StartWazeGame()
+    {
+        foreach(Light light in _lightsToFadeIn)
+        {
+            StartCoroutine(FadeLight(light));
+        }
+    }
+
+    IEnumerator FadeLight(Light light)
+    {
+        float step = 0;
+
+        while(step < 1)
+        {
+            light.range = Mathf.Lerp(0, _maxLightRange, step);
+
+            step += Time.deltaTime / _fadeTime;
+
+            yield return null;
+        }
+
+        yield return null;
+    }
+}
+
+[CustomEditor(typeof(WazerStartup))]
+class TestButton : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Test StartWazeGame"))
+        {
+            WazerStartup testClass = (WazerStartup)target;
+
+            testClass.StartWazeGame();
+        }
+    }
+}
