2f9cfccfb625a0a28c0411f44ebb431c53c3db32
kcrash201 <1008917@student.sae.edu.au>
2018-04-26 14:39:06 +0800

    Kyle Character/Placeholder audio for Wazer Scene

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Wazer/Robot Maid/MaidScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaidScript : MonoBehaviour {
+
+	private Animator _animator;
+	public AudioClip SoundToPlay;
+	public float Volume;
+	AudioSource audio;
+	public bool alreadyPlayed = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_animator = GetComponent<Animator> ();
+		audio = GetComponent<AudioSource> ();
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			_animator.SetBool ("Wave", true);
+			audio.PlayOneShot (SoundToPlay, Volume);
+		}
+		if (!alreadyPlayed) 
+		{
+			alreadyPlayed = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
