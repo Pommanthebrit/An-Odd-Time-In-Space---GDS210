549a07607456a1deb1ffa974cbeb034ba16cf18b
Chris-Stoj <1010691@student.sae.edu.au>
2018-04-27 03:52:49 +0800

    Jedi UI score + health, HUB tweaks

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Interfaces/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int myPlayerHealth;
+	SpriteRenderer _spriteRenderer;
+	public Sprite heartFull, heartEmpty;
+
+
+	void Start () {
+		_spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	void Update () {
+		if (myPlayerHealth == 0) {
+			_spriteRenderer.sprite = heartFull;
+		} else
+			_spriteRenderer.sprite = heartEmpty;
+	}
+}
