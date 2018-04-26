1361e89b7695e22a7da3d05201b2f7968f6d910e
Geurtsy <36181392+Geurtsy@users.noreply.github.com>
2018-04-27 04:46:57 +0800

    Health functionality added

---
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int _health;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "projectile")
+        {
+            _health--;
+            Debug.Log(_health);
+        }
+    }
+}
