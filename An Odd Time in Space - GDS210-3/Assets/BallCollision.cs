e67562968a69340d5b2775e9e81a7219cd2a0f54
CrimsonFury82 <1007626@student.sae.edu.au>
2018-04-26 15:44:23 +0800

    Aston
    Setup audio for various objects:
    Golf Tee Off
    Golf Bounce
    Golf Ambient Effects
    Laser Ambient Effects
    Laser Shoot

---
+++ An Odd Time in Space - GDS210-3/Assets/BallCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour {
+
+	private AudioSource _audioSource;
+
+	void Start () {
+		_audioSource = GetComponent<AudioSource>();
+	}
+
+	private void OnTriggerEnter(Collider col)
+	{
+		print ("bounce");
+		_audioSource.Play ();
+	}
+}
--- An Odd Time in Space - GDS210-3/Assets/Scripts/BallHit.cs
+++ An Odd Time in Space - GDS210-3/Assets/Scripts/BallHit.cs
@@ -8,10 +8,17 @@ public class BallHit : MonoBehaviour
 	[SerializeField] private float _torqueForce;
     [SerializeField] private float _pushRadius;
 
+	private AudioSource _audioSource;
+
+	void Start () {
+		_audioSource = GetComponent<AudioSource>();
+	}
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Ball")
         {
+			_audioSource.Play ();
             other.attachedRigidbody.AddExplosionForce(_hitForce, transform.position, _pushRadius);
 			other.attachedRigidbody.AddTorque (Vector3.forward, ForceMode.Impulse);
             Debug.Log(other);
